b83a53a9d9d87692bfecf1dd5dd4545eef57da83
Aurelian Rosca <aurelian@roscasend.com>
2021-07-08 09:31:20 +0300

    Spring beginner to guru first project

---
+++ spring5werbapp/src/main/java/com/roscasend/web/spring/guru/spring5werbapp/Spring5werbappApplication.java
@@ -0,0 +1,13 @@
+package com.roscasend.web.spring.guru.spring5werbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5werbappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5werbappApplication.class, args);
+    }
+
+}
---
+++ spring5werbapp/src/test/java/com/roscasend/web/spring/guru/spring5werbapp/Spring5werbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roscasend.web.spring.guru.spring5werbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5werbappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
